1a631cc86cab84cf39b61f8dc0eadc04fe8bb1c2
Sana Parveen V <sanaparveen1829@gmail.com>
2026-02-02 19:02:24 +0530

    DataIngestion

---
+++ main.py
@@ -0,0 +1,16 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+STAGE_NAME="Data Ingetsion stage"
+
+
+try:
+    logger.info(f">>>>>>{STAGE_NAME} started>>>>>>>>")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>{STAGE_NAME} completed>>>>>>>>")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
